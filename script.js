6096fc769f9ab314cfaeacec1eaae0b77d152d5d
Vaibhav8701 <112371740+Vaibhav8701@users.noreply.github.com>
2025-09-27 15:32:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,24 @@
-let weather = {
-  apiKey: "API KEY GOES HERE",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
-};
+const btn = document.getElementById("bt1")
+const input = document.getElementById("int")
 
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
-});
+const cityName = document.getElementById("city-name")
+const cityTime = document.getElementById("city-time")
+const cityTemp = document.getElementById("city-temp")
 
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
 
-weather.fetchWeather("Denver");
+
+
+async function getData(cityName) {
+   const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=8f3ffd9c7e28458b9ec83445252908&q=${cityName}&aqi=no`)
+   return  promise.json()
+}
+
+
+btn.addEventListener('click',async ()=>{
+    const value = input.value 
+    const result = await getData(value)
+    console.log(result)
+    cityName.innerText = `LOCATION : ${result.location.name}, ${result.location.region}, ${result.location.country}`
+    cityTime.innerText = `LOCALTIME : ${result.location.localtime}`
+    cityTemp.innerText = `TEMPERATURE : ${result.current.temp_c}`
+})
